4572c631196dc247038d53a20cc918713e0cb3ac
srikanthATB <1996srikanthb@gmail.com>
2024-12-29 21:06:26 +0530

    First class creation

---
+++ src/com/creatio/crm/language/basics/Class1.java
@@ -0,0 +1,16 @@
+package com.creatio.crm.language.basics;
+
+public class Class1 {
+	
+	String name = "Amit";
+	
+	int empId = 1234;
+	
+	boolean havingVisa = true; 
+	
+	public void printAddress() {
+		System.out.println("Jabalpur");
+		System.out.println("MP");
+	}
+
+}
